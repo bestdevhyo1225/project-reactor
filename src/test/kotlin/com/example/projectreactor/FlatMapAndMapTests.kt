d2a75ee32b739b1a772dbb0891dfb589b217724c
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-23 23:06:12 +0900

    test: map(), flatMap() 관련 테스트 코드 추가 (2)

--- src/test/kotlin/com/example/projectreactor/FlatMapAndMapTests.kt
+++ src/test/kotlin/com/example/projectreactor/FlatMapAndMapTests.kt
@@ -1,6 +1,7 @@
 package com.example.projectreactor
 
 import io.kotest.core.spec.style.DescribeSpec
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 internal class FlatMapAndMapTests : DescribeSpec({
@@ -27,6 +28,13 @@ internal class FlatMapAndMapTests : DescribeSpec({
                 }
             }
         }
+
+        it("문자열 변환 테스트") {
+            Flux.just("a", "bc", "def", "ghij")
+                .log()
+                .map { it.length }
+                .subscribe { consumer -> println("consumer = $consumer") }
+        }
     }
 
     describe("flatMap 메서드는") {
@@ -38,6 +46,24 @@ internal class FlatMapAndMapTests : DescribeSpec({
 
                 monoResult.subscribe { consumer -> println("consumer = $consumer") }
             }
+        }
+
+        it("Flux<Int> -> Flux<Int> 변환 테스트") {
+            Flux.just(1, 2, 3)
+                .flatMap {
+                    println("----- flatMap -----")
+                    Flux.range(1, it)
+                }
+                .subscribe { consumer -> println("consumer = $consumer") }
+        }
+
+        it("Flux<Int> -> Mono<Int> 변환 테스트") {
+            Flux.just(1, 2, 3)
+                .flatMap {
+                    println("----- flatMap -----")
+                    Mono.just(it * 2)
+                }
+                .subscribe { consumer -> println("consumer = $consumer") }
         }
     }
 })

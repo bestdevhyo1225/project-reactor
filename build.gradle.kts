3d79f55b869a52e5d4465d71bef86eb83c6bedbb
janghyoseog <bestdevhyo1225@gmail.com>
2022-05-27 21:22:04 +0900

    chore: 모듈 라이브러리 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,11 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("io.kotest:kotest-assertions-core:5.0.3")
-	testImplementation("io.kotest:kotest-assertions-core-jvm:5.0.3")
+	testImplementation("io.kotest:kotest-assertions-core:5.3.0")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.0")
 	testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
-	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.0.3")
-	testImplementation("io.mockk:mockk:1.12.1")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.3.0")
+	testImplementation("io.mockk:mockk:1.12.4")
 }
 
 tasks.withType<KotlinCompile> {
